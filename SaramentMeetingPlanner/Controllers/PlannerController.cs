5f897073c99e3560e933f10af389f5eee195f408
FiercelyPierce <cir21001@byui.edu>
2022-12-10 19:30:43 -0700

    Initial commit

---
+++ SaramentMeetingPlanner/Controllers/PlannerController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace SaramentMeetingPlanner.Controllers
+{
+    public class PlannerController : Controller
+    {
+        // 
+        // GET: /Planner/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /Planner/Welcome/ 
+        // Requires using System.Text.Encodings.Web;
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
