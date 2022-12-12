ec212245fd5f8b21b817c63a7946942e12a271ac
Troy Davidson <troymd2009@hotmail.com>
2022-12-12 13:59:46 -0700

    Updated home page

--- SaramentMeetingPlanner/wwwroot/js/site.js
+++ SaramentMeetingPlanner/wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+protected void btnPrint_Click(object sender, EventArgs e)
+{
+    Session["ctrl"] = pnl1;
+    ClientScript.RegisterStartupScript(this.GetType(), "onclick", "<script language=javascript>window.open('Print.aspx','PrintMe','height=300px,width=300px,scrollbars=1');</script>");
+}  
