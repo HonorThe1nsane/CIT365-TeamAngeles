4078729832e509c1f8fd989e2112937e782c4b0a
Troy Davidson <troymd2009@hotmail.com>
2022-12-13 23:58:43 -0700

    updated print page to be one page

---
+++ SaramentMeetingPlanner/Migrations/20221214063009_addSpeakers.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SaramentMeetingPlanner.Migrations
+{
+    /// <inheritdoc />
+    public partial class addSpeakers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
